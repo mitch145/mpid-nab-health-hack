5153900041e391802e466e7772c23fda9d2918cb
Mitch Ball <mitchb145@gmail.com>
2017-02-25 09:34:04 +1100

    Add Theme and fonts

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 import App from './App';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import Theme from './Theme';
 import './index.css';
 
+
 ReactDOM.render(
-  <App />,
+  <MuiThemeProvider muiTheme={Theme}>
+    <App />
+  </MuiThemeProvider>,
   document.getElementById('root')
 );
